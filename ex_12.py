932e8946bf4d1bb6e6962fc11125d8270829f474
Sidorenko Vladimir <info@svautoz.ru>
2023-07-30 13:03:51 +0300

    ex12 done

--- ex_12.py
+++ ex_12.py
@@ -2,20 +2,9 @@
 
 import random
 
-S = int(input("Введите сумму чисел: "))
-P = int(input("Введите произведение чисел: "))
+N = int(input("Введите N: "))
+k = 1
 
-
-60 = 50 + 10
-500 = 50 * 10
-
-side_1 = 0
-side_2 = 0
-for _ in range(0, n):
-    side = random.randint(0, 1)
-    print(side, end=' ')
-    if side:
-        side_1 += 1
-    else:
-        side_2 += 1
-print(f'\nМинимальное кол-во переворотов монеток равна {side_1 if side_1 <= side_2 else side_2}')
+while 2**k < N:
+    print(k, end=", ")
+    k += 1
